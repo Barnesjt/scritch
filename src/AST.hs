e8e4e0add49b6522c10080e03cd503b3b4fb286a
Jack Pattison <jepatt16@gmail.com>
2021-05-09 22:50:32 -0700

    more cleanup

--- src/AST.hs
+++ src/AST.hs
@@ -1,45 +1,35 @@
 module AST where
 
-type Prog = [Stmt]
-
 -- we're just applying functions to arguments
-data Stmt = App Function [Function]
+data Stmt = App Function Stmt | Base Basic
     deriving (Eq, Show)
 
 -- some basic data types
-data Basic = Obj Object
+data Basic = Obj String
            | Int Int
            | Bool Bool
            | Str String
     deriving (Eq, Show)
 
-data Function = Base Basic -- even basic data is a function (of 0 arguments probably)
-              | Op Operation -- not sure about this
-    deriving (Eq, Show)
-
-data Operation = Move -- right now there is no way of enforcing the number or type of function arguments
+data Function = Move Float Float -- Move a number of pixels in x and y directions
     deriving (Eq, Show)
 
 -- TODO figure out what objects are, and what to call them
--- for now we'll just keep track of position
-type Object = (Int, Int) -- placeholder
+-- for now we'll just keep track of position and a name
+type Object = (String, (Float, Float)) -- placeholder
 
 -- smart constructors
-obj :: (Int, Int) -> Function
+obj :: String -> Stmt
 obj = Base . Obj
 
-int :: Int -> Function
+int :: Int -> Stmt
 int = Base . Int
 
-bool :: Bool -> Function
+bool :: Bool -> Stmt
 bool = Base . Bool
 
-str :: String -> Function
+str :: String -> Stmt
 str = Base . Str
-
-testProg :: Prog
-testProg = [App (Op Move) [obj (0, 0), int 10]]
-
 
 -- Random Thoughts --
 
