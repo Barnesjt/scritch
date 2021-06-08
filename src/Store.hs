cbffad6c1baa7cadf55098d6616f462edcab9f3b
Jack Pattison <jepatt16@gmail.com>
2021-06-07 20:00:09 -0700

    cleanup

--- src/Store.hs
+++ src/Store.hs
@@ -1,17 +1,6 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module Store where
 
-import Control.Monad.Except   (ExceptT,MonadError,runExceptT,throwError)
-import Control.Monad.IO.Class (MonadIO,liftIO)
-import Control.Monad.Reader   (MonadReader,ask,local)
-import Control.Monad.RWS      (RWST,runRWST)
-import Control.Monad.State    (MonadState,get,put)
-import Control.Monad.Writer   (MonadWriter,tell)
-import Data.Functor.Identity  (Identity)
-
 import Data.Dynamic
-import Type.Reflection
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -26,12 +15,12 @@ x = toDyn (9 :: Int)
 y = fromDyn x "error"
 
 
-put :: Typeable a => String -> a -> State -> State
-put v x = Map.insert v $ toDyn x
+insert :: Typeable a => String -> a -> State -> State
+insert v x = Map.insert v $ toDyn x
 
-get :: Typeable a => String -> TypeDec a -> State -> Maybe a
-get v t s = case Map.lookup v s of
+query :: Typeable a => String -> TypeDec a -> State -> Maybe a
+query v t s = case Map.lookup v s of
     Just d -> fromDynamic d
     Nothing -> Nothing
 
-test = Map.insert "y" (toDyn "hello") $ Map.insert "x" (toDyn (9 :: Int)) Map.empty
+test = insert "y" "hello" $ insert "x" (9.0 :: Float) Map.empty
